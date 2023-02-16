cf85e7a0179a2bb92e37d7b8d12b178baa3acdcf
alecchhung <aleclavie@proton.me>
2023-02-16 11:44:07 +0800

    set up route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express()
 
+//Route
+app.get('/', (req, res) => {
+    res.send('Hello Home!')
+})
+
+
+//Port
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
