6642db43c9acce792ff10a8bcdd935fe99d227a2
alecchhung <aleclavie@proton.me>
2023-02-18 11:31:08 +0800

    created data scheme and model

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,30 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-//Route
+// Middleware
+app.use(express.json())
+
+// Route
 app.get('/', (req, res) => {
     res.send('Hello Home!')
 })
 
+// Create a todo task
+app.post('/api/todos', async (req, res) => {
+    console.log(req.body);
+    res.send('Todo Task Created.')
+})
 
-//Port
+// Port
 const PORT = process.env.PORT || 3000
 
+// connect to mongoose
 mongoose
     .set('strictQuery', false)
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+            console.log(`Server running on port ${PORT}.`);
         })
     })
     .catch((err) => console.log(err))
