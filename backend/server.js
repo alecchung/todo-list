718d053a34a9f6478420022a72cf3b657b2d59cc
alecchhung <aleclavie@proton.me>
2023-02-19 19:54:36 +0800

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Todo = require('./models/todoModel')
 const todoRoutes = require('./routes/todoRoute')
+const cors = require('cors')
 
 const app = express()
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors({
+    origin: 'http://localhost:3000/'
+}));
 app.use('/api/todos', todoRoutes)
 
 // Route
